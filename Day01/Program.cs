0659d77d2f6da9aec027c92c4fbe4eb000596ebd
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 16:13:16 -0600

    Day 01 Setup

---
+++ Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        static void SolvePart1()
+        {
+            string input = File.ReadAllText("Input.txt");
+            List<string> data = input.Split('\n').ToList();
+            Console.WriteLine(data);
+        }
+
+        static void SolvePart2()
+        {
+            string input = File.ReadAllText("Input.txt");
+            List<string> data = input.Split('\n').ToList();
+            Console.WriteLine(data);
+        }
+    }
+}
