5ad1f9fb8367f325a25db5c529012fa1db18b585
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 17:21:23 -0600

    Day 01 Part 2 Complete

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -89,7 +89,6 @@ namespace Day01
                         return;
                 }
             }
-
             Console.WriteLine("X = " + x + " Y = " + y);
             Console.WriteLine("Total blocks = " + (Math.Abs(x) + Math.Abs(y)));
         }
@@ -98,7 +97,120 @@ namespace Day01
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine(data);
+            var locs = new HashSet<(int, int)>();
+                        var moves = data[0].Split(" ").Select(s => s.Trim(',')).ToList();
+            var dir = 'N';
+            int x = 0, y = 0;
+            var found = false;
+            foreach (var m in moves)
+            {
+                switch (m[0])
+                {
+                    case 'R':
+                        switch (dir)
+                        {
+                            case 'N':
+                                dir = 'E';
+                                break;
+                            case 'E':
+                                dir = 'S';
+                                break;
+                            case 'S':
+                                dir = 'W';
+                                break;
+                            case 'W':
+                                dir = 'N';
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                return;
+                        }
+
+                        break;
+                    case 'L':
+                        switch (dir)
+                        {
+                            case 'N':
+                                dir = 'W';
+                                break;
+                            case 'E':
+                                dir = 'N';
+                                break;
+                            case 'S':
+                                dir = 'E';
+                                break;
+                            case 'W':
+                                dir = 'S';
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke!");
+                        return;
+                }
+
+                var num = int.Parse((m[1..]));
+                switch (dir)
+                {
+                    case 'N':
+                        for (var i = 0; i < num; i++)
+                        {
+                            y += 1;
+                            if (locs.Contains((x, y))) 
+                            {
+                                found = true;
+                                break;
+                            }
+                            locs.Add((x, y));
+                        }
+                        break;
+                    case 'E':
+                        for (var i = 0; i < num; i++)
+                        {
+                            x += 1;
+                            if (locs.Contains((x, y))) 
+                            {
+                                found = true;
+                                break;
+                            }
+                            locs.Add((x, y));
+                        }
+                        break;
+                    case 'S':
+                        for (var i = 0; i < num; i++)
+                        {
+                            y -= 1;
+                            if (locs.Contains((x, y))) 
+                            {
+                                found = true;
+                                break;
+                            }
+                            locs.Add((x, y));
+                        }
+                        break;
+                    case 'W':
+                        for (var i = 0; i < num; i++)
+                        {
+                            x -= 1;
+                            if (locs.Contains((x, y))) 
+                            {
+                                found = true;
+                                break;
+                            }
+                            locs.Add((x, y));
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke!");
+                        return;
+                }
+                if (found) break;
+            }
+            Console.WriteLine("X = " + x + " Y = " + y);
+            Console.WriteLine("Repeated location blocks = " + (Math.Abs(x) + Math.Abs(y)));
         }
     }
 }
