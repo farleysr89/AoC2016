9ad82230a57a6a2697da039fec250535c72c3a26
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 17:53:17 -0600

    Day 02 Part 2 Complete

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -16,12 +16,11 @@ namespace Day02
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            var begin = 5;
+            const int begin = 5;
             var code = "";
-            foreach (var s in data)
+            var current = begin;
+            foreach (var s in data.Where(s => s != ""))
             {
-                var current = begin;
-                if (s == "") continue;
                 foreach (var c in s)
                 {
                     switch (c)
@@ -42,20 +41,153 @@ namespace Day02
                             if (current > 6) break;
                             current += 3;
                             break;
-                        default:
-                            break;
                     }
                 }
                 code += current.ToString();
             }
 
-            Console.WriteLine("Door Code is " + code);
+            Console.WriteLine($"Door Code is {code}");
         }
 
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
+            const char begin = '5';
+            var code = "";
+            var current = begin;
+            foreach (var s in data.Where(s => s != ""))
+            {
+                foreach (var c in s)
+                {
+                    switch (c)
+                    {
+                        case 'U':
+                            switch (current)
+                            {
+                                case '3':
+                                    current = '1';
+                                    break;
+                                case '6':
+                                    current = '2';
+                                    break;
+                                case '7':
+                                    current = '3';
+                                    break;
+                                case '8':
+                                    current = '4';
+                                    break;
+                                case 'A':
+                                    current = '6';
+                                    break;
+                                case 'B':
+                                    current = '7';
+                                    break;
+                                case 'C':
+                                    current = '8';
+                                    break;
+                                case 'D':
+                                    current = 'B';
+                                    break;
+                            }
+
+                            break;
+                        case 'R':
+                            switch (current)
+                            {
+                                case '2':
+                                    current = '3';
+                                    break;
+                                case '3':
+                                    current = '4';
+                                    break;
+                                case '5':
+                                    current = '6';
+                                    break;
+                                case '6':
+                                    current = '7';
+                                    break;
+                                case '7':
+                                    current = '8';
+                                    break;
+                                case '8':
+                                    current = '9';
+                                    break;
+                                case 'A':
+                                    current = 'B';
+                                    break;
+                                case 'B':
+                                    current = 'C';
+                                    break;
+                            }
+
+                            break;
+                        case 'L':
+                            switch (current)
+                            {
+                                case '3':
+                                    current = '2';
+                                    break;
+                                case '4':
+                                    current = '3';
+                                    break;
+                                case '6':
+                                    current = '5';
+                                    break;
+                                case '7':
+                                    current = '6';
+                                    break;
+                                case '8':
+                                    current = '7';
+                                    break;
+                                case '9':
+                                    current = '8';
+                                    break;
+                                case 'B':
+                                    current = 'A';
+                                    break;
+                                case 'C':
+                                    current = 'B';
+                                    break;
+                            }
+
+                            break;
+                        case 'D':
+                            switch (current)
+                            {
+                                case '1':
+                                    current = '3';
+                                    break;
+                                case '2':
+                                    current = '6';
+                                    break;
+                                case '3':
+                                    current = '7';
+                                    break;
+                                case '4':
+                                    current = '8';
+                                    break;
+                                case '6':
+                                    current = 'A';
+                                    break;
+                                case '7':
+                                    current = 'B';
+                                    break;
+                                case '8':
+                                    current = 'C';
+                                    break;
+                                case 'B':
+                                    current = 'D';
+                                    break;
+                            }
+
+                            break;
+                    }
+                }
+                code += current;
+            }
+
+            Console.WriteLine($"Door Code is {code}");
         }
     }
 }
