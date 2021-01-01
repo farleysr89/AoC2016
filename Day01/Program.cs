71d98f5d788361a57e7dbca647b2533e3c0a73f2
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 16:37:36 -0600

    Day 01 Part 1 Complete

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -17,7 +17,81 @@ namespace Day01
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine(data);
+            var moves = data[0].Split(" ").Select(s => s.Trim(',')).ToList();
+            var dir = 'N';
+            int x = 0, y = 0;
+            foreach (var m in moves)
+            {
+                switch (m[0])
+                {
+                    case 'R':
+                        switch (dir)
+                        {
+                            case 'N':
+                                dir = 'E';
+                                break;
+                            case 'E':
+                                dir = 'S';
+                                break;
+                            case 'S':
+                                dir = 'W';
+                                break;
+                            case 'W':
+                                dir = 'N';
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                return;
+                        }
+
+                        break;
+                    case 'L':
+                        switch (dir)
+                        {
+                            case 'N':
+                                dir = 'W';
+                                break;
+                            case 'E':
+                                dir = 'N';
+                                break;
+                            case 'S':
+                                dir = 'E';
+                                break;
+                            case 'W':
+                                dir = 'S';
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke!");
+                        return;
+                }
+
+                switch (dir)
+                {
+                    case 'N':
+                        y += int.Parse(m[1..]);
+                        break;
+                    case 'E':
+                        x += int.Parse(m[1..]);
+                        break;
+                    case 'S':
+                        y -= int.Parse(m[1..]);
+                        break;
+                    case 'W':
+                        x -= int.Parse(m[1..]);
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke!");
+                        return;
+                }
+            }
+
+            Console.WriteLine("X = " + x + " Y = " + y);
+            Console.WriteLine("Total blocks = " + (Math.Abs(x) + Math.Abs(y)));
         }
 
         private static void SolvePart2()
