799948f23724a53d985ae587ae42b89cf37499cf
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 20:32:02 -0600

    Day 04 Part 2 Complete

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -17,7 +17,7 @@ namespace Day04
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            int idSum = 0;
+            var idSum = 0;
             foreach (var s in data.Where(s => s != ""))
             {
                 var counts = new Dictionary<char, int>();
@@ -41,7 +41,41 @@ namespace Day04
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            //var idSum = 0;
+            var rooms = new List<(string, int)>();
+            foreach (var s in data.Where(s => s != ""))
+            {
+                var counts = new Dictionary<char, int>();
+                var id = int.Parse(s[(s.LastIndexOf('-') + 1)..s.IndexOf('[')]);
+                var line = s.Split('[');
+                var room = line[0][0..s.LastIndexOf('-')].Replace("-", "");
+                var checkSum = line[1].Trim(']');
+                foreach (var c in room)
+                {
+                    if (counts.ContainsKey(c)) counts[c]++;
+                    else counts[c] = 1;
+                }
+
+                var topFive = counts.OrderByDescending(kv => kv.Value).ThenBy(kv => kv.Key).Take(5).Select(kv => kv.Key).Aggregate("", (current, next) => current + next);
+                if (topFive == checkSum) rooms.Add((s[0..s.LastIndexOf('-')], id));
+            }
+
+            var decryptedRooms = new List<(string, int)>();
+            foreach (var r in rooms)
+            {
+                var decrypted = "";
+                var inc = r.Item2 % 26;
+                foreach (var c in r.Item1)
+                {
+                    if (c == '-') decrypted += " ";
+                    else
+                    {
+                        decrypted += (c + inc) > 122 ? (char)((c + inc) - 26) : (char)(c + inc);
+                    }
+                }
+                decryptedRooms.Add((decrypted, r.Item2));
+            }
+            Console.WriteLine("North Pole objects are in room " + decryptedRooms.FirstOrDefault(r => r.Item1.Contains("north")).Item2);
         }
     }
 }
