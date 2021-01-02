9f0faa1611a62021c5a66573f1d4cc46505f05ac
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-02 17:13:48 -0600

    Day 09 Part 1 Complete

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -16,7 +16,27 @@ namespace Day09
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var s = data[0];
+            var ss = "";
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    var ii = s[i..].IndexOf(')');
+                    var sss = s.Substring(i + 1, ii - 1);
+                    var dims = sss.Split(("x"));
+                    var letterCount = int.Parse(dims[0]);
+                    var multiCount = int.Parse(dims[1]);
+                    var stringToMulti = s[(i + ii + 1)..(i + ii + letterCount + 1)];
+                    ss += MultiplyString(stringToMulti, multiCount);
+                    i += ii + letterCount;
+                }
+                else
+                {
+                    ss += s[i];
+                }
+            }
+            Console.WriteLine("Decrypted Length = " + ss.Length);
         }
 
         private static void SolvePart2()
@@ -24,6 +44,17 @@ namespace Day09
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
             Console.WriteLine("");
+        }
+
+        private static string MultiplyString(string s, int count)
+        {
+            var ss = "";
+            for (var i = 0; i < count; i++)
+            {
+                ss += s;
+            }
+
+            return ss;
         }
     }
 }
