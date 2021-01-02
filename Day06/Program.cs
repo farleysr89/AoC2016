d77a3a4ecffb9f727ab6755bd1d947de8580d713
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-02 14:31:35 -0600

    Day 06 Part 1 Complete

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -16,7 +17,22 @@ namespace Day06
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            List<Dictionary<char, int>> counts = new List<Dictionary<char, int>>();
+            foreach (var s in data)
+            {
+                if (s == "") continue;
+                var index = 0;
+                foreach (var c in s)
+                {
+                    if (counts.ElementAtOrDefault(index) == null) counts.Add(new Dictionary<char, int>());
+                    if (counts[index].ContainsKey(c)) counts[index][c]++;
+                    else counts[index].Add(c, 1);
+                    index++;
+                }
+            }
+
+            var password = counts.Aggregate("", (current, d) => current + d.First(x => x.Value == d.Max(y => y.Value)).Key);
+            Console.WriteLine("Password = " + password);
         }
 
         private static void SolvePart2()
