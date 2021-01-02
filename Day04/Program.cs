cd166b74e55f7d643b33a71e5cc91e0ac85cf372
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 19:59:36 -0600

    Day 04 Part 1 Complete

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -16,7 +17,24 @@ namespace Day04
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            int idSum = 0;
+            foreach (var s in data.Where(s => s != ""))
+            {
+                var counts = new Dictionary<char, int>();
+                var id = int.Parse(s[(s.LastIndexOf('-') + 1)..s.IndexOf('[')]);
+                var line = s.Split('[');
+                var room = line[0][0..s.LastIndexOf('-')].Replace("-", "");
+                var checkSum = line[1].Trim(']');
+                foreach (var c in room)
+                {
+                    if (counts.ContainsKey(c)) counts[c]++;
+                    else counts[c] = 1;
+                }
+
+                var topFive = counts.OrderByDescending(kv => kv.Value).ThenBy(kv => kv.Key).Take(5).Select(kv => kv.Key).Aggregate("", (current, next) => current + next);
+                if (topFive == checkSum) idSum += id;
+            }
+            Console.WriteLine("Valid rooms = " + idSum);
         }
 
         private static void SolvePart2()
