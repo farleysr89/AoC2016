9ff6a8f1ee3059bf18d4120898ecf7717d93fea6
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 19:03:02 -0600

    Day 03 Part 2 Complete

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -16,7 +17,7 @@ namespace Day03
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            var count = data.Where(s => s != "").Select(s => s.Trim().Split(" ").Where(c => c != "").Select(int.Parse).OrderBy(x => x).ToList()).Count(sides => sides[0] + sides[1] > sides[2]);
+            var count = data.Where(s => s != "").Select(s => s.Split(" ").Where(c => c != "").Select(int.Parse).OrderBy(x => x).ToList()).Count(sides => sides[0] + sides[1] > sides[2]);
             Console.WriteLine("Valid triangles count = " + count);
         }
 
@@ -24,7 +25,20 @@ namespace Day03
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var count = 0;
+            for (var i = 0; i < data.Count; i += 3)
+            {
+                var line1 = data[i].Split(" ").Where(c => c != "").Select(int.Parse).ToList();
+                var line2 = data[i + 1].Split(" ").Where(c => c != "").Select(int.Parse).ToList();
+                var line3 = data[i + 2].Split(" ").Where(c => c != "").Select(int.Parse).ToList();
+                var col1 = new List<int> { line1[0], line2[0], line3[0] }.OrderBy(x => x).ToList();
+                var col2 = new List<int> { line1[1], line2[1], line3[1] }.OrderBy(x => x).ToList();
+                var col3 = new List<int> { line1[2], line2[2], line3[2] }.OrderBy(x => x).ToList();
+                if (col1[0] + col1[1] > col1[2]) count++;
+                if (col2[0] + col2[1] > col2[2]) count++;
+                if (col3[0] + col3[1] > col3[2]) count++;
+            }
+            Console.WriteLine("Valid triangles count = " + count);
         }
     }
 }
