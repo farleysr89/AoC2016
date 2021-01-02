9c5c3887cc29e59ff76dc44e27bd8418ad6b0b64
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-01 18:40:35 -0600

    Day 03 Part 1 Complete

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,29 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Day03
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             SolvePart1();
             SolvePart2();
         }
 
-        static void SolvePart1()
+        private static void SolvePart1()
         {
-            string _input = File.ReadAllText("Input.txt");
-            List<string> data = _input.Split('\n').ToList();
-            Console.WriteLine("");
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            var count = data.Where(s => s != "").Select(s => s.Trim().Split(" ").Where(c => c != "").Select(int.Parse).OrderBy(x => x).ToList()).Count(sides => sides[0] + sides[1] > sides[2]);
+            Console.WriteLine("Valid triangles count = " + count);
         }
 
-        static void SolvePart2()
+        private static void SolvePart2()
         {
-            string _input = File.ReadAllText("Input.txt");
-            List<string> data = _input.Split('\n').ToList();
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
             Console.WriteLine("");
         }
     }
