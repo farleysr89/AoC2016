3a7b29718d76207d12e09a5c905136ab63ab03f5
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-04 19:23:39 -0600

    Day 11 Complete

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -16,14 +16,37 @@ namespace Day11
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var itemCount = 0;
+            var moveCount = 0;
+            int floor = 1;
+            foreach (var s in data)
+            {
+                if (s == "" || floor == 4) continue;
+                var parts = s.Replace(",", "").Replace(".", "").Split((" "));
+                itemCount += parts.Count(x => x == "generator" || x == "microchip");
+                moveCount += itemCount * 2 - 3;
+                floor++;
+            }
+            Console.WriteLine("Minimum moves = " + moveCount);
         }
 
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var itemCount = 0;
+            var moveCount = 0;
+            int floor = 1;
+            foreach (var s in data)
+            {
+                if (s == "" || floor == 4) continue;
+                if (floor == 1) itemCount += 4;
+                var parts = s.Replace(",", "").Replace(".", "").Split((" "));
+                itemCount += parts.Count(x => x == "generator" || x == "microchip");
+                moveCount += itemCount * 2 - 3;
+                floor++;
+            }
+            Console.WriteLine("Minimum moves = " + moveCount);
         }
     }
 }
