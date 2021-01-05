d14125d544711cabeaee8d2963b49b47d2ed848c
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-04 22:48:58 -0600

    Day 14 Part 2 Complete

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -8,6 +8,7 @@ namespace Day14
 {
     internal class Program
     {
+        private static Dictionary<string, string> hashes = new Dictionary<string, string>();
         private static void Main()
         {
             SolvePart1();
@@ -56,8 +57,68 @@ namespace Day14
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var salt = input.Split('\n')[0];
+            var index = 0;
+            var count = 0;
+            var goal = 64;
+            while (count < goal)
+            {
+                string hex;
+                if (hashes.ContainsKey(salt + index))
+                {
+                    hex = hashes[salt + index];
+                }
+                else
+                {
+                    hex = GetHash(salt + index);
+                }
+
+                for (var i = 0; i < hex.Length - 2; i++)
+                {
+                    var found = false;
+                    if (hex[i] != hex[i + 1] || hex[i] != hex[i + 2]) continue;
+                    var tmp = index + 1;
+                    var tmpMax = index + 1001;
+                    while (tmp < tmpMax)
+                    {
+                        string tmpHex;
+                        if (hashes.ContainsKey(salt + tmp))
+                        {
+                            tmpHex = hashes[salt + tmp];
+                        }
+                        else
+                        {
+                            tmpHex = GetHash(salt + tmp);
+                        }
+
+                        if (tmpHex.Contains(hex[i].ToString() + hex[i] + hex[i] + hex[i] + hex[i]))
+                        {
+                            found = true;
+                            break;
+                        }
+                        tmp++;
+                    }
+
+                    if (found) count++;
+                    break;
+                }
+
+                index++;
+            }
+            Console.WriteLine("64th key index = " + (index - 1));
+        }
+
+        public static string GetHash(string s)
+        {
+            var md5 = MD5.Create();
+            var ss = s;
+            for (var i = 0; i < 2017; i++)
+            {
+                var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(ss));
+                ss = BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+            hashes.Add(s, ss);
+            return ss;
         }
     }
 }
