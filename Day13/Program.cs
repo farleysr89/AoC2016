208454dc7d817ca009aee08c1a68ef7de0f29d34
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-04 20:44:37 -0600

    Day 13 Part 1
    Created map and solved manually

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -15,7 +15,23 @@ namespace Day13
         private static void SolvePart1()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
+            var code = int.Parse(input.Split('\n')[0]);
+            for (var x = 0; x < 50; x++)
+            {
+                var line = "";
+                for (var y = 0; y < 50; y++)
+                {
+                    if (x == 1 && y == 1) line += "&";
+                    else if (x == 31 && y == 39) line += "E";
+                    else
+                    {
+                        int num = x * x + 3 * x + 2 * x * y + y + y * y + code;
+                        var bin = Convert.ToString(num, 2);
+                        line += bin.Count(c => c == '1') % 2 == 0 ? "-" : "#";
+                    }
+                }
+                Console.WriteLine(line);
+            }
             Console.WriteLine("");
         }
 
