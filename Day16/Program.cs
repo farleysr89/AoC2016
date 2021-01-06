18c571a924b50adf9b7637516ddbbe633add5134
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-05 18:39:13 -0600

    Day 16 Part 1 Complete

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
+using File = System.IO.File;
 
 namespace Day16
 {
@@ -16,7 +16,31 @@ namespace Day16
         {
             var input = File.ReadAllText("Input.txt");
             var initial = input.Split('\n')[0];
-            Console.WriteLine("");
+            var length = 272;
+            while (initial.Length < length)
+            {
+                var a = new string(initial);
+                var bA = a.ToCharArray();
+                Array.Reverse(bA);
+                var b = new string(bA);
+                b = b.Replace('0', 'a').Replace('1', '0').Replace('a', '1');
+                initial = a + '0' + b;
+            }
+
+            string checkSum;
+            while (true)
+            {
+                checkSum = "";
+                for (var i = 0; i < Math.Min(length, initial.Length); i += 2)
+                {
+                    checkSum += initial[i] == initial[i + 1] ? "1" : "0";
+                }
+
+                if (checkSum.Length % 2 == 1) break;
+                initial = checkSum;
+            }
+
+            Console.WriteLine("CheckSum = " + checkSum);
         }
 
         private static void SolvePart2()
