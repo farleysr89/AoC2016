4196775da355eb13b781de7ed2dc8fa4c7ffe1de
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-05 18:19:21 -0600

    Day 16 Setup

---
+++ Day16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day16
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var initial = input.Split('\n')[0];
+            Console.WriteLine("");
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
