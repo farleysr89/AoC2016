f5c27948eebdf7e197e61403a6373d832a65e3ab
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-07 22:22:07 -0600

    Day 22 Setup

---
+++ Day22/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day22
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            var nodes = new List<Node>();
+            foreach (var s in data.Where(s => s[0] == '/'))
+            {
+                var parts = s.Split(" ");
+            }
+            Console.WriteLine("");
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+
+    internal class Node
+    {
+        public int X;
+        public int Y;
+        public int Size;
+        public int Used;
+        public int Available;
+        public int UsedPercent;
+    }
+}
