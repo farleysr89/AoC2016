15cafa249a7ef7a0306c39fb8582780a1dedae49
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-08 20:56:55 -0600

    Update Program.cs

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -17,19 +16,25 @@ namespace Day22
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            var nodes = new List<Node>();
-            foreach (var s in data.Where(s => s != "").Where(s => s[0] == '/'))
-            {
-                var parts = s.Split(" ").Where(p => p != "").ToArray();
-                var path = parts[0].Split('-');
-                var x = int.Parse(path[1][1..]);
-                var y = int.Parse(path[2][1..]);
-                var size = int.Parse(parts[1][0..^1]);
-                var used = int.Parse(parts[2][0..^1]);
-                var available = int.Parse(parts[3][0..^1]);
-                var usedPercent = int.Parse(parts[4][0..^1]);
-                nodes.Add(new Node { X = x, Y = y, Size = size, Used = used, Available = available, UsedPercent = usedPercent });
-            }
+            var nodes = (from s in data.Where(s => s != "").Where(s => s[0] == '/')
+                         select s.Split(" ").Where(p => p != "").ToArray()
+            into parts
+                         let path = parts[0].Split('-')
+                         let x = int.Parse(path[1][1..])
+                         let y = int.Parse(path[2][1..])
+                         let size = int.Parse(parts[1][0..^1])
+                         let used = int.Parse(parts[2][0..^1])
+                         let available = int.Parse(parts[3][0..^1])
+                         let usedPercent = int.Parse(parts[4][0..^1])
+                         select new Node
+                         {
+                             X = x,
+                             Y = y,
+                             Size = size,
+                             Used = used,
+                             Available = available,
+                             UsedPercent = usedPercent
+                         }).ToList();
 
             var count = (from n in nodes.Where(x => x.Used > 0) from nn in nodes.Where(x => x.X != n.X || x.Y != n.Y) where n.Used <= nn.Available select n).Count();
             Console.WriteLine("Valid pairs = " + count);
@@ -39,27 +44,31 @@ namespace Day22
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            var nodes = new List<Node>();
-            foreach (var s in data.Where(s => s != "").Where(s => s[0] == '/'))
-            {
-                var parts = s.Split(" ").Where(p => p != "").ToArray();
-                var path = parts[0].Split('-');
-                var x = int.Parse(path[1][1..]);
-                var y = int.Parse(path[2][1..]);
-                var size = int.Parse(parts[1][0..^1]);
-                var used = int.Parse(parts[2][0..^1]);
-                var available = int.Parse(parts[3][0..^1]);
-                var usedPercent = int.Parse(parts[4][0..^1]);
-                nodes.Add(new Node { X = x, Y = y, Size = size, Used = used, Available = available, UsedPercent = usedPercent });
-            }
+            var nodes = (from s in data.Where(s => s != "").Where(s => s[0] == '/')
+                         select s.Split(" ").Where(p => p != "").ToArray()
+            into parts
+                         let path = parts[0].Split('-')
+                         let x = int.Parse(path[1][1..])
+                         let y = int.Parse(path[2][1..])
+                         let size = int.Parse(parts[1][0..^1])
+                         let used = int.Parse(parts[2][0..^1])
+                         let available = int.Parse(parts[3][0..^1])
+                         let usedPercent = int.Parse(parts[4][0..^1])
+                         select new Node
+                         {
+                             X = x,
+                             Y = y,
+                             Size = size,
+                             Used = used,
+                             Available = available,
+                             UsedPercent = usedPercent
+                         }).ToList();
 
             var startX = nodes.Max(n => n.X);
-            var startY = 0;
-            var goalX = 0;
-            var goalY = 0;
+            const int startY = 0;
             var print = "";
-            int dataSize = nodes.First(n => n.X == startX && n.Y == startY).Used;
-            int currY = 0;
+            var dataSize = nodes.First(n => n.X == startX && n.Y == startY).Used;
+            var currY = 0;
             var maxFree = nodes.Max(n => n.Available);
             nodes = nodes.OrderBy(n => n.Y).ThenBy(n => n.X).ToList();
             foreach (var n in nodes)
@@ -73,8 +82,6 @@ namespace Day22
 
                 print += n.Used > maxFree ? "#" : dataSize > n.Available ? "|" : "_";
             }
-            //var count = (from n in nodes.Where(x => x.Used > 0) from nn in nodes.Where(x => x.X != n.X || x.Y != n.Y) where n.Used <= nn.Available select n).Count();
-            //Console.WriteLine("Valid pairs = " + count);
         }
     }
 
