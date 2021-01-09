f433a97e8abd5f4c6bd6727dcf051646d55ccbed
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-08 22:26:41 -0600

    Day 23 Part 2
    Still need to optimize

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace Day23
 {
@@ -235,8 +234,222 @@ namespace Day23
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split('\n');
+            long a = 12, b = 0, c = 0, d = 0, index = 0;
+            while (true)
+            {
+                if (index >= data.Length) break;
+                var s = data[index];
+                if (s == "") break;
+                var parts = s.Split(" ");
+                long x = 0;
+                switch (parts[0])
+                {
+                    case "cpy":
+                        if (int.TryParse(parts[1], out var i))
+                        {
+                            switch (parts[2][0])
+                            {
+                                case 'a':
+                                    a = i;
+                                    break;
+                                case 'b':
+                                    b = i;
+                                    break;
+                                case 'c':
+                                    c = i;
+                                    break;
+                                case 'd':
+                                    d = i;
+                                    break;
+                                default:
+                                    Console.WriteLine("Something Broke!");
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            switch (parts[1][0])
+                            {
+                                case 'a':
+                                    x = a;
+                                    break;
+                                case 'b':
+                                    x = b;
+                                    break;
+                                case 'c':
+                                    x = c;
+                                    break;
+                                case 'd':
+                                    x = d;
+                                    break;
+                                default:
+                                    Console.WriteLine("Something Broke!");
+                                    break;
+                            }
+                            switch (parts[2][0])
+                            {
+                                case 'a':
+                                    a = x;
+                                    break;
+                                case 'b':
+                                    b = x;
+                                    break;
+                                case 'c':
+                                    c = x;
+                                    break;
+                                case 'd':
+                                    d = x;
+                                    break;
+                                default:
+                                    Console.WriteLine("Something Broke!");
+                                    break;
+                            }
+                        }
+
+                        break;
+                    case "inc":
+                        switch (parts[1][0])
+                        {
+                            case 'a':
+                                a++;
+                                break;
+                            case 'b':
+                                b++;
+                                break;
+                            case 'c':
+                                c++;
+                                break;
+                            case 'd':
+                                d++;
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                break;
+                        }
+                        break;
+                    case "dec":
+                        switch (parts[1][0])
+                        {
+                            case 'a':
+                                a--;
+                                break;
+                            case 'b':
+                                b--;
+                                break;
+                            case 'c':
+                                c--;
+                                break;
+                            case 'd':
+                                d--;
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                break;
+                        }
+                        break;
+                    case "jnz":
+                        long y = 0;
+                        if (int.TryParse(parts[2], out var j))
+                        {
+                            y = j;
+                        }
+                        else
+                        {
+                            switch (parts[2][0])
+                            {
+                                case 'a':
+                                    y = a;
+                                    break;
+                                case 'b':
+                                    y = b;
+                                    break;
+                                case 'c':
+                                    y = c;
+                                    break;
+                                case 'd':
+                                    y = d;
+                                    break;
+                                default:
+                                    Console.WriteLine("Something Broke!");
+                                    break;
+                            }
+                        }
+
+                        y--;
+                        if (int.TryParse(parts[1], out var h))
+                        {
+                            if (h == 0) continue;
+                            index += y;
+                        }
+                        else
+                        {
+                            switch (parts[1][0])
+                            {
+                                case 'a':
+                                    if (a != 0) index += y;
+                                    break;
+                                case 'b':
+                                    if (b != 0) index += y;
+                                    break;
+                                case 'c':
+                                    if (c != 0) index += y;
+                                    break;
+                                case 'd':
+                                    if (d != 0) index += y;
+                                    break;
+                                default:
+                                    Console.WriteLine("Something Broke!");
+                                    break;
+                            }
+                        }
+                        break;
+                    case "tgl":
+                        switch (parts[1][0])
+                        {
+                            case 'a':
+                                x = a;
+                                break;
+                            case 'b':
+                                x = b;
+                                break;
+                            case 'c':
+                                x = c;
+                                break;
+                            case 'd':
+                                x = d;
+                                break;
+                            default:
+                                Console.WriteLine("Something Broke!");
+                                break;
+                        }
+
+                        if (index + x >= data.Length) break;
+                        var modInst = data[index + x].Split(" ");
+                        string newCommand;
+                        string newInstruction;
+                        if (modInst.Length == 2)
+                        {
+                            newCommand = modInst[0] == "inc" ? "dec" : "inc";
+                            newInstruction = newCommand + " " + modInst[1];
+                        }
+                        else if (modInst.Length == 3)
+                        {
+                            newCommand = modInst[0] == "jnz" ? "cpy" : "jnz";
+                            newInstruction = newCommand + " " + modInst[1] + " " + modInst[2];
+                        }
+                        else break;
+
+                        data[index + x] = newInstruction;
+                        break;
+                    default:
+                        Console.WriteLine("Something Broke!");
+                        return;
+                }
+
+                index++;
+            }
+            Console.WriteLine("value in a = " + a);
         }
     }
 }
